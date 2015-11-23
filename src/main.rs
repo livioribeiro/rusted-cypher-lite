6154210f933a5df5f08deb89d18ef66c4f90a156
Livio Ribeiro <livioribeiro@outlook.com>
2015-11-23 10:26:54 -0300

    removed unecessary file

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-extern crate rusted_cypher;
-
-use rusted_cypher::GraphClient;
-
-fn main() {
-    let client = GraphClient::connect("http://neo4j:neo4j@localhost:7474/db/data").unwrap();
-
-    println!("{:?}", client.neo4j_version());
-}
